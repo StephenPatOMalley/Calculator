bc8bd91cdada86b0a834321235497bfc707caf63
StephenPatOMalley <omalleypatstephen@gmail.com>
2022-02-02 09:31:54 +0000

    Maven with JUint

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -2,17 +2,19 @@ package ie.gmit;
 
 public class Calculator {
 
-    public static int Add(int firstNum, int secondNum){
+    public int Add(int firstNum, int secondNum){
 
         int addedNum = 0;
         addedNum = firstNum + secondNum;
 
         return addedNum;
     }
-    public static void main(String args[]) {
-        int num1 = 3, num2 = 5, added = 0;
 
-        added = Add(num1, num2);
-        System.out.println(added);
+    public int SubTract(int firstNum, int secondNum){
+        int subNum = 0;
+        subNum = firstNum - secondNum;
+
+        return subNum;
     }
+
 }
---
+++ src/test/java/ie/gmit/CalculatorTest.java
@@ -0,0 +1,9 @@
+package ie.gmit;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    void justAnExample() {
+    }
+}
